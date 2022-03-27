e2deb86e20673aeae78f017a2e0cdd925b10d221
roma_marshall <roma_marshall@outlook.com>
2022-03-27 20:16:06 +0200

    Init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-red-300">Tailwind CSS with Vue 3 and Vite</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
