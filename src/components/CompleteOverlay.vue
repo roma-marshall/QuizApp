e0010828112057844e0f1e6d09d34e436465b13f
roma_marshall <roma_marshall@outlook.com>
2022-04-03 18:00:14 +0200

    end overlay edit

--- src/components/CompleteOverlay.vue
+++ src/components/CompleteOverlay.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="w-screen h-screen absolute z-30 bg-white bg-black/50 flex justify-center items-center">
-    <div class="bg-green-700/80 p-4 rounded-lg text-center text-white">
+    <div class="bg-green-700/80 p-7 rounded-lg text-center text-white">
       <p class="font-bold text-2xl">All done!</p>
       <p class="my-4 font-bold text-3xl">100% score</p>
       <div class="flex justify-around">
-        <div class="rounded-full py-1 w-28 border cursor-pointed hover:text-black hover:bg-white">Done</div>
+        <div class="rounded-full py-1 w-28 border cursor-pointed hover:text-black hover:bg-white mr-3">Done</div>
         <div class="rounded-full py-1 w-28 border cursor-pointed hover:text-black hover:bg-white">Retry</div>
       </div>
     </div>
